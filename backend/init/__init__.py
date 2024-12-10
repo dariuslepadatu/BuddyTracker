10c483178f50150c175e84187aca7bf843afa421
darius <darius.lepadatu@stud.acs.upb.ro>
2024-12-10 21:09:42 +0200

    added config file

--- backend/init/__init__.py
+++ backend/init/__init__.py
@@ -1,42 +1,24 @@
-import os
-
-import redis
-from dotenv import load_dotenv
 from flask import Flask
-
+from init.config import Config
+import redis
 from init.instances import cors
 
-# Load environment variables from .env file
-load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    # Load configuration from environment variables or config file
-    app.config['KEYCLOAK_URL'] = os.getenv('KEYCLOAK_URL')
-    app.config['KEYCLOAK_REALM'] = os.getenv('KEYCLOAK_REALM')
-    app.config['KEYCLOAK_CLIENT_ID'] = os.getenv('KEYCLOAK_CLIENT_ID')
-    app.config['KEYCLOAK_CLIENT_SECRET'] = os.getenv('KEYCLOAK_CLIENT_SECRET')
-    app.config['KEYCLOAK_ADMIN_USERNAME'] = os.getenv('KEYCLOAK_ADMIN_USERNAME')
-    app.config['KEYCLOAK_ADMIN_PASSWORD'] = os.getenv('KEYCLOAK_ADMIN_PASSWORD')
-    app.config['INTROSPECT_URL'] = f"{os.getenv('KEYCLOAK_URL')}/realms/{os.getenv('KEYCLOAK_REALM')}/protocol/openid-connect/token/introspect"
-    app.config['TOKEN_URL'] = f"{os.getenv('KEYCLOAK_URL')}/realms/{os.getenv('KEYCLOAK_REALM')}/protocol/openid-connect/token"
-    app.config['USER_URL'] = f"{ os.getenv('KEYCLOAK_URL')}/admin/realms/{ os.getenv('KEYCLOAK_REALM')}/users"
-    # Import and register blueprints
-    app.config['REDIS_HOST'] = os.getenv('REDIS_HOST', 'localhost')  # Default to localhost
-    app.config['REDIS_PORT'] = os.getenv('REDIS_PORT', 6379)  # Default Redis port
-    app.config['REDIS_PASSWORD'] = os.getenv('REDIS_PASSWORD', None)  # Default to None if no password is set
 
-    # Setup Redis connection
+    app.config.from_object(Config)
+
     redis_client = redis.StrictRedis(
-        host=app.config['REDIS_HOST'],
-        port=app.config['REDIS_PORT'],
-        password=app.config['REDIS_PASSWORD'],
-        db=0,  # Select DB 0 by default
-        decode_responses=True  # Automatically decode responses as strings
+        host=Config.REDIS_HOST,
+        port=Config.REDIS_PORT,
+        password=Config.REDIS_PASSWORD,
+        db=0,
+        decode_responses=True
     )
 
     app.redis = redis_client
-    cors.init_app(app)  # Allow all origins
+    cors.init_app(app)
 
     from api.ops_redis import ops_db
     app.register_blueprint(ops_db, url_prefix='/db')
---
+++ backend/init/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    # Define your environment variables here
+    KEYCLOAK_URL = os.getenv('KEYCLOAK_URL')
+    KEYCLOAK_REALM = os.getenv('KEYCLOAK_REALM')
+    KEYCLOAK_CLIENT_ID = os.getenv('KEYCLOAK_CLIENT_ID')
+    KEYCLOAK_CLIENT_SECRET = os.getenv('KEYCLOAK_CLIENT_SECRET')
+    KEYCLOAK_ADMIN_USERNAME = os.getenv('KEYCLOAK_ADMIN_USERNAME')
+    KEYCLOAK_ADMIN_PASSWORD = os.getenv('KEYCLOAK_ADMIN_PASSWORD')
+    INTROSPECT_URL = f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token/introspect"
+    TOKEN_URL = f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token"
+    USER_URL = f"{KEYCLOAK_URL}/admin/realms/{KEYCLOAK_REALM}/users"
+    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+    REDIS_PORT = os.getenv('REDIS_PORT', 6379)
+    REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
