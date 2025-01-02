a489beaa365e4af205017a228cc99e0306fb306f
darius <darius.lepadatu@stud.acs.upb.ro>
2025-01-02 15:40:08 +0200

    search groups with debounce added

--- frontend/src/screens/groups/GroupsScreen.tsx
+++ frontend/src/screens/groups/GroupsScreen.tsx
@@ -1,55 +1,88 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {Button, SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import {useFocusEffect} from "@react-navigation/native";
-import {getGroups, validate} from "../../helpers/backend_helper.ts";
-import {Text, Surface, Searchbar} from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
+import { getGroups } from "../../helpers/backend_helper.ts";
+import { Text, Surface, Searchbar } from "react-native-paper";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const GroupsScreen = () => {
     const tabBarHeight = useBottomTabBarHeight();
     const [groups, setGroups] = useState([]);
-    const [searchQuery, setSearchQuery] = React.useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [isLoading, setIsLoading] = useState(false); // Stare pentru loader
+
     useFocusEffect(
-        React.useCallback(() => {
+        useCallback(() => {
             getGroups({})
                 .then((response) => {
                     setGroups(response.groups);
                 });
         }, [])
     );
+
     useEffect(() => {
-        getGroups({search_query: searchQuery})
-            .then((response) => {
-                setGroups(response.groups);
-            });
+        setIsLoading(true);
+        const delayDebounceFn = setTimeout(() => {
+                getGroups({ search_query: searchQuery })
+                    .then((response) => {
+                        setGroups(response.groups);
+                        setIsLoading(false);
+                    })
+                    .catch(() => setIsLoading(false));
+        }, 500);
+
+        return () => {
+            clearTimeout(delayDebounceFn);
+        };
     }, [searchQuery]);
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.searchRow}>
                 <Searchbar
+                    icon={ () => <Icon
+                        name="search"
+                        size={20}
+                    />}
                     style={styles.searchbar}
                     placeholder="Search"
                     onChangeText={setSearchQuery}
                     value={searchQuery}
+                    clearIcon={() =>
+                        searchQuery ? (
+                            <Icon
+                                name="close"
+                                size={20}
+                            />
+                        ) : null
+                    }
+                    onClearIconPress={() => setSearchQuery('')}
                 />
+
                 <Icon name="plus-square-o" size={25} style={styles.icon} />
             </View>
-            <ScrollView contentContainerStyle={[styles.scrollview, { paddingBottom: tabBarHeight }]}>
-                {groups.length > 0 ? (
-                    groups.map((group, idx) => (
-                        <Surface key={idx} style={styles.surface} elevation={4}>
-                            <Text style={styles.groupText}>
-                                {group}
-                            </Text>
-                        </Surface>
-                    ))
-                ) : (
-                    <View style={styles.emptyContainer}>
-                        <Text style={styles.emptyText}>No groups available</Text>
-                    </View>
-                )}
-            </ScrollView>
+            {isLoading ? (
+                <View style={styles.loaderContainer}>
+                    <ActivityIndicator size="large" color="#C03BDE" />
+                </View>
+            ) : (
+                <ScrollView contentContainerStyle={[styles.scrollview, { paddingBottom: tabBarHeight }]}>
+                    {groups.length > 0 ? (
+                        groups.map((group, idx) => (
+                            <Surface key={idx} style={styles.surface} elevation={4}>
+                                <Text style={styles.groupText}>
+                                    {group}
+                                </Text>
+                            </Surface>
+                        ))
+                    ) : (
+                        <View style={styles.emptyContainer}>
+                            <Text style={styles.emptyText}>No groups available</Text>
+                        </View>
+                    )}
+                </ScrollView>
+            )}
         </SafeAreaView>
     );
 };
@@ -70,12 +103,12 @@ const styles = StyleSheet.create({
     searchRow: {
         flexDirection: 'row',
         alignItems: 'center',
-        width: '70%', // Fixează lățimea totală la 80% din ecran
-        alignSelf: 'center', // Centrează pe ecran
+        width: '70%',
+        alignSelf: 'center',
         marginBottom: 20,
     },
     searchbar: {
-        flex: 1, // Face Searchbar să ocupe cât mai mult spațiu
+        flex: 1,
         marginRight: 10,
     },
     icon: {
@@ -105,6 +138,11 @@ const styles = StyleSheet.create({
     emptyText: {
         fontSize: 18,
         color: '#999',
+    },
+    loaderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
 
